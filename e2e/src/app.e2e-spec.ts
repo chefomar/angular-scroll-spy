765090cad7de81d61fc7bc9dadbbbbf482a2178b
chefomar <elshinnaweyom@gmail.com>
2019-08-16 04:52:10 +0300

    write e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,45 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, until, by } from 'protractor';
 
-describe('workspace-project App', () => {});
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(async () => {
+    page = new AppPage();
+    await page.navigateTo();
+    await page.setWindowSize();
+  });
+
+  it('should have first link active', async () => {
+    const activeLinks = await page.getActiveNavLinks();
+
+    expect(activeLinks.length).toEqual(1);
+    expect(activeLinks[0]).toEqual('target 1');
+  });
+
+  it('should still have first link active', async () => {
+    await page.scrollBy(50);
+    const activeLinks = await page.getActiveNavLinks();
+
+    expect(activeLinks.length).toEqual(1);
+    expect(activeLinks[0]).toEqual('target 1');
+  });
+
+  it('should have second link active', async () => {
+    await page.scrollBy(51);
+    await browser.wait(until.elementLocated(by.css('.navbar__link--active[spyOn=target-2]')), 1000);
+    const activeLinks = await page.getActiveNavLinks();
+
+    expect(activeLinks.length).toEqual(1);
+    expect(activeLinks[0]).toEqual('target 2');
+  });
+
+  it('should have second link active', async () => {
+    await page.clickNavLink('target-2');
+    await browser.wait(until.elementLocated(by.css('.navbar__link--active[spyOn=target-2]')), 1000);
+    const activeLinks = await page.getActiveNavLinks();
+
+    expect(activeLinks.length).toEqual(1);
+    expect(activeLinks[0]).toEqual('target 2');
+  });
+});
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,31 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
+  private readonly windowHeight = 1000;
+  private readonly windowWidth = 1000;
+
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get(browser.baseUrl);
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  clickNavLink(id: string) {
+    return element(by.css(`.navbar__link[href="#${id}"]`)).click();
+  }
+
+  scrollBy(scroll: number) {
+    return browser.executeScript(`window.scrollTo(0, ${scroll});`);
+  }
+
+  getNavLink(name: string) {
+    return element(by.css(`[spyOn=${name}]`)).getText();
+  }
+
+  getActiveNavLinks() {
+    return element.all(by.css('.navbar__link--active')).map(link => link.getText());
+  }
+
+  setWindowSize() {
+    browser.driver.manage().window().setSize(this.windowWidth, this.windowHeight);
   }
 }
