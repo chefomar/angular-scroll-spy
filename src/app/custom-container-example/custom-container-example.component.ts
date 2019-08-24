bef58e29b1c5adda422faaa9204f7e6a7722dfcc
chefomar <elshinnaweyom@gmail.com>
2019-08-25 00:01:53 +0300

    update example

--- src/app/custom-container-example/custom-container-example.component.ts
+++ src/app/custom-container-example/custom-container-example.component.ts
@@ -9,10 +9,27 @@ import { ScrollSpyService } from 'ng-spy';
 })
 export class CustomContainerExampleComponent implements OnInit, AfterViewInit {
   @Output() private activeTarget = new EventEmitter();
+  @ViewChild('target5', {static: true}) target5: ElementRef;
+  @ViewChild('target6', {static: true}) target6: ElementRef;
 
   constructor(private element: ElementRef, private scrollSpyService: ScrollSpyService) { }
 
   ngOnInit() {
+    this.addTargets();
+  }
+
+  destroySpy() {
+    this.scrollSpyService.stopSpying();
+  }
+
+  addTargets() {
+    this.scrollSpyService.addTarget({ name: 'target-5', element: this.target5 });
+    this.scrollSpyService.addTarget({ name: 'target-6', element: this.target6 });
+  }
+
+  spyAgain() {
+    this.addTargets();
+    this.scrollSpyService.spy({ scrollContainer: this.element });
   }
 
   ngAfterViewInit() {
